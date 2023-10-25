8adfbc2fbe21d08f4e91263f1a58cf0963aea053
Dinh Thuat <dinhthuatdp@gmail.com>
2023-10-25 23:50:43 +0700

    add code

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public static Dictionary<string, float> Scores = new();
+
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI txtScore;
+    [SerializeField] private Button btnClickMe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 0;
+        txtScore.text = "0";
+        btnClickMe.onClick.RemoveAllListeners();
+        btnClickMe.onClick.AddListener(SliderChange);
+    }
+
+    public void SliderChange()
+    {
+        slider.value += 1;
+        txtScore.text = slider.value.ToString();
+    }
+}
---
+++ Assets/Scripts/TabController.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static Unity.Burst.Intrinsics.X86.Avx;
+
+public class TabController : MonoBehaviour
+{
+    [SerializeField] private Button btnTab1;
+    [SerializeField] private Button btnTab2;
+    [SerializeField] private Image tab1Content;
+    [SerializeField] private Image tab2Content;
+    [SerializeField] private Image itemTemplate;
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI txtScore;
+    [SerializeField] private ScrollRect scrollRects;
+    [SerializeField] private GridLayoutGroup gridLayoutGroup;
+    [SerializeField] private TextMeshProUGUI txtSelectedName;
+    [SerializeField] private TextMeshProUGUI txtSelectedScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeTab(1);
+        btnTab1.onClick.RemoveAllListeners();
+        btnTab1.onClick.AddListener(Tab1Click);
+        btnTab2.onClick.RemoveAllListeners();
+        btnTab2.onClick.AddListener(Tab2Click);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Tab1Click()
+    {
+        ChangeTab(1);
+    }
+
+    private void Tab2Click()
+    {
+        ChangeTab(2);
+    }
+
+    private void ChangeTab(int tabIndex)
+    {
+        switch (tabIndex)
+        {
+            case 1:
+                ShowHideTab2Content(false);
+                ShowHideTab1Content(true);
+                break;
+            case 2:
+                ShowHideTab1Content(false);
+                ShowHideTab2Content(true);
+                break;
+        }
+    }
+
+    private void ShowHideTab1Content(bool isShow)
+    {
+        if (isShow)
+        {
+            txtSelectedName.text = string.Empty;
+            txtSelectedScore.text = string.Empty;
+            var score = slider.value;
+            if (score != 0)
+            {
+                AddScores(score);
+                slider.value = 0.0f;
+                txtScore.text = "0";
+            }
+            SetLeaderBoard();
+        }
+        tab1Content.gameObject.SetActive(isShow);
+    }
+
+    private void ShowHideTab2Content(bool isShow)
+    {
+        if (isShow)
+        {
+        }
+        tab2Content.gameObject.SetActive(isShow);
+    }
+
+    private void AddScores(float score)
+    {
+        ScoreController.Scores.Add(Guid.NewGuid().ToString("n").Substring(0, 6), score);
+    }
+
+    private void SetLeaderBoard()
+    {
+        // Destroy all item is list.
+        for (int i = 0; i < gridLayoutGroup.transform.childCount; i++)
+        {
+            Destroy(gridLayoutGroup.transform.GetChild(i).gameObject);
+        }
+        
+        if (ScoreController.Scores == null ||
+            !ScoreController.Scores.Any())
+        {
+            return;
+        }
+        var topRank = ScoreController.Scores.OrderByDescending(x => x.Value)
+            .Take(100)
+            .ToList();
+        float itemHight = 60;
+        float spacingY = 5;
+        foreach (var s in topRank.Select((v, i) => (v, i)))
+        {
+            var obj = Instantiate(itemTemplate);
+            var comp = obj.GetComponent<Image>(); // gameObject (item list view)
+            if (comp != null)
+            {
+                var no = comp.transform.Find("txtNo").GetComponent<TextMeshProUGUI>();
+                var name = comp.transform.Find("txtName").GetComponent<TextMeshProUGUI>();
+                var score = comp.transform.Find("txtScore").GetComponent<TextMeshProUGUI>();
+                var btn = comp.transform.Find("Button").GetComponent<Button>();
+                no.text = $"{s.i + 1}";
+                name.text = s.v.Key;
+                score.text = s.v.Value.ToString();
+                btn.onClick.AddListener(delegate { ItemClick(obj, no, name, score); });
+            }
+            obj.gameObject.SetActive(true);
+            obj.transform.SetParent(gridLayoutGroup.transform, false);
+        }
+        var totalItems = gridLayoutGroup.transform.childCount;
+        var newContentHeight = itemHight * totalItems + spacingY * totalItems;
+        scrollRects.content.sizeDelta = new Vector2(scrollRects.content.sizeDelta.x, newContentHeight);
+        scrollRects.content.anchoredPosition = new Vector3(scrollRects.content.anchoredPosition.x, newContentHeight * 0.5f);
+        scrollRects.verticalNormalizedPosition = 1;
+    }
+
+    private Image prevSelected;
+
+    private void ItemClick(Image obj,
+        TextMeshProUGUI no,
+        TextMeshProUGUI name,
+        TextMeshProUGUI score)
+    {
+        if (prevSelected != null)
+        {
+            var noPrev = prevSelected.transform.Find("txtNo").GetComponent<TextMeshProUGUI>();
+            var namePrev = prevSelected.transform.Find("txtName").GetComponent<TextMeshProUGUI>();
+            var scorePrev = prevSelected.transform.Find("txtScore").GetComponent<TextMeshProUGUI>();
+            prevSelected.color = new Color32(236, 207, 106, 255);
+            var textColor = Color.white;
+            noPrev.color = textColor;
+            namePrev.color = textColor;
+            scorePrev.color = textColor;
+        }
+        prevSelected = obj;
+        txtSelectedName.text = name.text;
+        txtSelectedScore.text = score.text;
+        var textSelectedColor = Color.black;
+        obj.color = Color.cyan;
+        no.color = textSelectedColor;
+        name.color = textSelectedColor;
+        score.color = textSelectedColor;
+    }
+}
